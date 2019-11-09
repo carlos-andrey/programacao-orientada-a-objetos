f8f7013eb8e02c9cd30d343e2da5d3472c1a5883
Andrey Alves <carlos.aas2001@gmail.com>
2019-11-08 21:14:05 -0300

    Funcionalidades basicas

--- listas/lista-de-exercicio-04/queimadas-na-amazonia.py
+++ listas/lista-de-exercicio-04/queimadas-na-amazonia.py
@@ -1,28 +1,38 @@
 arquivo_amazon = open('amazon.csv')
 
-casos_acre = 0	
-casos_ceara = 0
-casos_amazonas = 0
-casos_mato_grosso = 0
+entrada_estado = input('Informe estado de interesse: ')
+entrada_ano = input('Informe ano de interesse: ')
+queimadas = 0
+
+
 for linha in arquivo_amazon:
 	linha = linha.strip('\n')
 	ano, estado, mes, numero, data = linha.split(',')
 	numero = numero.replace(".", "")
 	estado= estado.replace('"','')
-	if estado == 'Acre' and ano == '2015':
-		casos_acre = casos_acre + int(numero)
+	while estado == entrada_estado and ano == entrada_ano:
+		queimadas = queimadas + int(numero)
 
-	elif estado == 'Ceara' and ano == '2014':
-		casos_ceara = casos_ceara + int(numero)
+		
+	
 
-	elif estado == 'Amazonas':
-		casos_amazonas = casos_amazonas + int(numero)
+print('As queimadas em {} no ano {} foram {}'.format(entrada_estado, entrada_ano, queimadas))
+arquivo_amazon = open('amazon.csv')
 
-	elif estado == 'Mato Grosso':
-		if int(ano) >= 2010: 
-			casos_mato_grosso = casos_mato_grosso + int(numero)
+entrada_estado = input('Informe estado de interesse: ')
+entrada_ano = input('Informe ano de interesse: ')
+queimadas = 0
 
-print("Queimadas no Acre em 2015: {}".format(casos_acre))		
-print("Queimadas no Ceara em 2014: {}".format(casos_ceara))
-print("Queimadas no Amazonas: {}".format(casos_amazonas))
-print("Queimadas no Mato Grosso ate de 2010 : {}".format(casos_mato_grosso))
+
+for linha in arquivo_amazon:
+	linha = linha.strip('\n')
+	ano, estado, mes, numero, data = linha.split(',')
+	numero = numero.replace(".", "")
+	estado= estado.replace('"','')
+	while estado == entrada_estado and ano == entrada_ano:
+		queimadas = queimadas + int(numero)
+
+		
+	
+
+print('As queimadas em {} no ano {} foram {}'.format(entrada_estado, entrada_ano, queimadas))
