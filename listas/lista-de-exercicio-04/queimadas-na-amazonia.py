fb70c74bdc6b5739823dbbe03ace25e444ef07cc
Andrey Alves <carlos.aas2001@gmail.com>
2019-11-08 21:23:45 -0300

    Adicionando funcionalidades iterativas

--- listas/lista-de-exercicio-04/queimadas-na-amazonia.py
+++ listas/lista-de-exercicio-04/queimadas-na-amazonia.py
@@ -17,22 +17,3 @@ for linha in arquivo_amazon:
 	
 
 print('As queimadas em {} no ano {} foram {}'.format(entrada_estado, entrada_ano, queimadas))
-arquivo_amazon = open('amazon.csv')
-
-entrada_estado = input('Informe estado de interesse: ')
-entrada_ano = input('Informe ano de interesse: ')
-queimadas = 0
-
-
-for linha in arquivo_amazon:
-	linha = linha.strip('\n')
-	ano, estado, mes, numero, data = linha.split(',')
-	numero = numero.replace(".", "")
-	estado= estado.replace('"','')
-	while estado == entrada_estado and ano == entrada_ano:
-		queimadas = queimadas + int(numero)
-
-		
-	
-
-print('As queimadas em {} no ano {} foram {}'.format(entrada_estado, entrada_ano, queimadas))
