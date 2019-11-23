18158cb0931021b0ed1dc62abeb33ecc1aedb168
Andrey Alves <carlos.aas2001@gmail.com>
2019-11-23 15:57:59 -0300

    Refazendo questão 2.

--- listas/lista-de-exercicio-05/q2.py
+++
@@ -1,8 +0,0 @@
-def sequencia(numero,numeros):
-  for linha in range(1, numero + 1) :
-    numeros.append(linha)
-    print(*numeros)
-numero = int(input('Digite um numero : '))
-numeros = []
-
-sequencia(numero, numeros)
