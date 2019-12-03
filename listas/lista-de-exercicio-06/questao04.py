950f7d2dbff1b9301a46451676f9350e98c80fd9
daviaquino87 <daviaquino87@gmail.com>
2019-12-03 00:02:48 -0300

    Resolvendo a questão 4.

---
+++ listas/lista-de-exercicio-06/questao04.py
@@ -0,0 +1,17 @@
+def calcula_media(nomes, notas):
+    with open(nomes, 'r') as alunos:
+        nome_aluno = alunos.readlines()
+        with open(notas, 'r') as notas:
+            lista_notas = notas.readlines()
+            with open('medias.txt', 'w') as notas_medias:
+                for linha in range(0, len(lista_notas)):
+                    media = lista_notas[linha].split(' ')
+                    soma_final = 0
+                    for elemento in range(0, len(media)):
+                        soma = media[elemento].replace('"', ' ')                           
+                        soma_final += int(soma)
+                    print('{} {:.2f}'.format(nome_aluno[linha].strip('\n'), soma_final / len(soma)), file = notas_medias)
+x = input('Informe nome do arquivo de alunos: ')
+y = input('Informe nome do arquivo de notas: ')
+
+calcula_media(x, y)
