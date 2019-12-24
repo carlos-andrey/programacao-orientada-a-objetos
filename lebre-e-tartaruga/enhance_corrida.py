710ec424382ee4fe10fb3962610cabd1fe153058
Carlos Andrey <carlos.aas2001@gmail.com>
2019-12-24 15:45:17 -0300

    iniciando corrida entre lebre e tartaruga, utilzando objetos.

--- lebre-e-tartaruga/enhance_corrida.py
+++ lebre-e-tartaruga/enhance_corrida.py
@@ -6,24 +6,29 @@ class Tartaruga:
         self.posicao = posicao
     def caminhar_rapidamente(self):
         self.posicao += 3
+        return self.posicao
     def escorrega(self):
         self.posicao -= 6
+        return self.posicao
     def caminha_lentamente(self):
         self.posicao += 1
+        return self.posicao
 class Lebre:
     def __init__(self, cor, posicao):
         self.cor = cor
         self.posicao = posicao
     def dorme(self):
         self.posicao += 0
+        return self.posicao
     def salto_grande(self):
         self.posicao += 9
+        return self.posicao
     def escorrega_bastante(self):
         self.posicao -= 12
+        return self.posicao
     def salto_pequeno(self):
         self.posicao += 1
+        return self.posicao
     def escorrega_pouco(self):
         self.posicao -= 2        
-i = 0
-tar1 = Tartaruga('verde', 1)
-leb1 = Tartaruga('branco', 1)
+        return self.posicao
